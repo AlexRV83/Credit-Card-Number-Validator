a2832025a387e1bdfaa5a1a1cc955a106ba50df7
AlexRV83 <85925901+AlexRV83@users.noreply.github.com>
2021-07-14 09:19:00 +0300

    correction of the report

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "375792499829786";
+        String number = "30494357095727";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
@@ -35,4 +35,3 @@ public class Main {
         return (result != 0) && (result % 10 == 0);
     }
 }
-
